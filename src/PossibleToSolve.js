e3438f1d0b2aa720cff10b40ada7842ce0e58583
andreasspilde <81984194+andreasspilde@users.noreply.github.com>
2024-05-15 10:36:53 +0200

    Fikset sjekken for losbarhet

--- src/PossibleToSolve.js
+++ src/PossibleToSolve.js
@@ -1,21 +1,16 @@
 export default function PossibleToSolve(outArray) {
     let sum = 0
     for(let i = 0; i < outArray.length; i++){
-        console.log(outArray[i][1]+ ' vi ser på')
         if(outArray[i][1] === 15){
-            if(outArray[i][1] > 3){
-                sum += 1;
-            }
-            if(i > 5){
-                sum += 1;
-            }
-            if(i > 7){
-                sum += 1;
-            }
             if(i > 11){
+                sum += 3;
+            }
+            else if(i > 7){
+                sum += 2;
+            }
+            else if(i > 3){
                 sum += 1;
             }
-            console.log(sum + ' summen nå')
         }
         else{
             for(let j = i + 1; j < outArray.length; j++){
@@ -24,7 +19,6 @@ export default function PossibleToSolve(outArray) {
                     console.log(outArray[j][1])
                 }
             }
-            console.log(sum + ' er summen')
         }
     }
     if((sum % 2) === 1){
